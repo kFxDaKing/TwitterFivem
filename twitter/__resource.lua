be27e492a9fe823c28f7f2819c577568bef07fb1
jadou7 <42550761+jadou7@users.noreply.github.com>
2019-11-16 13:30:26 +0100

    Delete __resource.lua

--- twitter/__resource.lua
+++
@@ -1,5 +0,0 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
-description 'Relier la commande de twitter in-game Fivem à un salon discord. Script original créé par super merguez#3817'
-
-server_script 'server.lua'
